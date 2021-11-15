df3eb97a0b6746f136917bc9023e6106b528a5b5
Christin Xaviour <christinxaviour@gmail.com>
2021-11-16 01:55:37 +0530

    deploying angular-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { DatesheetComponent } from './datesheet/datesheet.component';
+import { GradesComponent } from './grades/grades.component';
+import { ResultComponent } from './result/result.component';
+import { SeatingComponent } from './seating/seating.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatesheetComponent,
+    GradesComponent,
+    ResultComponent,
+    SeatingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'App',
+      component: AppComponent},
+      {path: 'grades',
+      component: GradesComponent},
+      {path: 'datesheet',
+      component: DatesheetComponent},
+      {path: 'result',
+      component: ResultComponent},
+      {path: 'seating',
+      component: SeatingComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/datesheet/datesheet.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DatesheetComponent } from './datesheet.component';
+
+describe('DatesheetComponent', () => {
+  let component: DatesheetComponent;
+  let fixture: ComponentFixture<DatesheetComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DatesheetComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DatesheetComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
